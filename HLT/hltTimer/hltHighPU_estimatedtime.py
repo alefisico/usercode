78ff94f14fb3463a8e88d296307676a5caab0ff6
algomez <>
2011-08-10 06:37:14 +0000

    version with aditional plot function

--- HLT/hltTimer/hltHighPU_estimatedtime.py
+++ HLT/hltTimer/hltHighPU_estimatedtime.py
@@ -11,8 +11,8 @@
 #    pile up events (2).
 # 3. From (2) takes paths-modules and mean time for each.
 # 4. From (1) takes number events, max number events and time path.
-# 5. Matches paths and for each path freq = (number of events)/(max events).
-# 6. Multiplies freq*mean time from (2) = estimated time.
+# 5. Matches paths and for each path weight = (number of events)/(max events).
+# 6. Multiplies weight*mean time from (2) = estimated time.
 # 7. Returns two repos (dictionaties) with pathnames:meantime and pathnames:estimatedtime
 # 8. This script now takes some paths from pathlist, but is easy to modify for the whole menu.
 # 9. Plots the difference between nominal time and estimated time.
@@ -140,7 +140,7 @@ def get_highPU_time(repo_means,repo_nentries,maxEvents):
 		for modname_nentries, nentries in repo_nentries[pathname_nentries].iteritems():
 			freq = nentries/maxEvents
 			for pathname_means in repo_means:
-				for modname_means, mean in repo_means[pathname_nentries].iteritems():
+				for modname_means, mean in repo_means[pathname_means].iteritems():
 					if (pathname_nentries == pathname_means) and (modname_means == modname_nentries):
 						newtime = freq*mean 
 						if not pathname_nentries in repo_newtime:
@@ -205,6 +205,65 @@ def plot_results(repo_PathTime, repo_estimatedPathTime, pathlist):#, file1, file
 	del can
 
 
+##############################################################
+def plot_results_permodule(repoNominalPathTime, repoHighPUPathTime, repoEstimatedPathTime, pathlist):
+	''' Plot module time per Path '''
+##############################################################             
+        maxmean = 0
+        for path in pathlist:
+                can = TCanvas("can","",1500,800);
+                can.cd();
+                for pathname1 in repoNominalPathTime:
+                        if path == pathname1:
+                		h1 = TH1F("h1",path, len(repoNominalPathTime[pathname1]),0,len(repoNominalPathTime[pathname1]))
+                                for modname1, time1 in repoNominalPathTime[pathname1].iteritems():
+                                        h1.Fill(modname1, time1)
+#                                        print pathname1, modname1, time1
+
+                for pathname2 in repoHighPUPathTime:
+                        if path == pathname2:
+                		h2 = TH1F("h2","", len(repoHighPUPathTime[pathname2]),0,len(repoHighPUPathTime[pathname2]))
+                                for modname2, time2 in repoHighPUPathTime[pathname2].iteritems():
+                                        h2.Fill(modname2, time2)
+                                        if time2 > maxmean:
+                                                 maxmean = time2
+#                                        print pathname2, modname2, time2
+
+                for pathname3 in repoEstimatedPathTime:
+                        if path == pathname3:
+                		h3 = TH1F("h3","", len(repoEstimatedPathTime[pathname3]),0,len(repoEstimatedPathTime[pathname3]))
+                                for modname3, time3 in repoEstimatedPathTime[pathname3].iteritems():
+                                        h3.Fill(modname3, time3)
+#                                        print pathname3, modname3, time3
+
+	h1.Draw()
+	h1.SetLineWidth(2)
+	h1.SetMaximum(maxmean+10)
+	h1.GetYaxis().SetTitle("msec")
+	h1.GetXaxis().SetLabelSize(0.03)
+	h2.SetLineColor(2)
+	h2.Draw("same")
+	h2.SetLineWidth(2)
+	h3.SetLineColor(3)
+	h3.Draw("same")
+	h3.SetLineWidth(2)
+	can.SetBottomMargin(0.35)
+	can.SetGridx()
+	can.SetGridy()
+	lg = TLegend(0.79, 0.89, 1.0, 0.99);
+	lg.AddEntry(h1,"Nominal","L");
+	lg.AddEntry(h2,"High Pile Up","L");
+	lg.AddEntry(h3,"Estimated","L");
+	lg.SetTextSize(0.025);
+	lg.SetBorderSize(0);
+	lg.SetFillColor(0);
+	lg.Draw("same");
+	can.SaveAs(path+".png")
+	del can
+
+
+
+
 ###############################################################
 def main():
 ###############################################################
@@ -233,21 +292,33 @@ def main():
 		sys.exit(1)
 
 	#get the histos info in containers 
-	pathlist = ['HLT_Ele32_WP70_PFMT50_v3',
-		    'HLT_Mu40_v5',
-		    'HLT_Jet300_v5',
-		    'HLT_Photon26_IsoVL_Photon18_IsoVL_v7',
-		    'HLT_HT300_MHT80_v2']
+#	pathlist = ['HLT_Mu40_v5']
+#	pathlist = ['HLT_Ele32_WP70_PFMT50_v3']
+	pathlist = ['HLT_Jet300_v5']
+#	pathlist = ['HLT_Photon26_IsoVL_Photon18_IsoVL_v7']
+#	pathlist = ['HLT_HT300_MHT80_v2']
+#	pathlist = ['HLT_Mu40_v5',
+#		'HLT_Ele32_WP70_PFMT50_v3',
+#		'HLT_Jet300_v5',
+#		'HLT_Photon26_IsoVL_Photon18_IsoVL_v7',
+#		'HLT_HT300_MHT80_v2']
+
 	
 	print "This process takes a while.... please be patient\n"
 
 	repo1 = get_histos_info_mean(infile1)
+	repo5 = get_histos_info_mean(infile2)		#for test porpouses
 	repo2, repo3, nentries = get_histos_info_nominal(infile2,repo1)
 	time1, repo4 = get_highPU_time(repo1,repo2, nentries)
+	#print repo1
+	#print repo5
+	#print time1 
 
 	#plot results
-	plot_results(repo3,repo4, pathlist)
-	print "Final plot is hltHighPU_estimatedtime.png\n"
+#	plot_results(repo3,repo4, pathlist)
+	plot_results_permodule(repo5,repo1, time1, pathlist)
+#	print "Final plot is hltHighPU_estimatedtime.png\n"
+	print "Final plot is pathname.png where pathname is the specific path name\n"
 
 #######################################################
 if __name__ =='__main__':
