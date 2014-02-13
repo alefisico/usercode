6a574892b8fc15cd9474ddcab69d602365793828
Alejandro <gomez@rutgers.physics.edu>
2014-02-13 11:11:12 -0600

    new version

--- Stops/RUAnalysis/MyAnalyzer/test/makeFit_WS_data.py
+++ Stops/RUAnalysis/MyAnalyzer/test/makeFit_WS_data.py
@@ -31,8 +31,6 @@ FitEnd = 2000
 
 ####### RooFit Variables
 x = RooRealVar("x","Quadruplet Invariant Mass [GeV]",0.,2000.)
-quadruplets = RooArgList( x )
-x.setRange(FitStart,FitEnd)
 
 
 #########################################################
@@ -40,35 +38,45 @@ x.setRange(FitStart,FitEnd)
 #########################################################
 def fitP4toData(histo, folder, file, outputDir, WSname, WSDir):
 
+	#x.setRange(FitStart,FitEnd)
+	quadruplets = RooArgList( x )
+
 	##### Background (P4 function)
-	p1 = RooRealVar("p1","p1", -113.6,-1000,1000) 		########################## This values are from previous fits, but any value works.
-	p2 = RooRealVar("p2","p2", 56.39,-100,100)
-	p3 = RooRealVar("p3","p3", 8.31,-100,100)
+	#p1 = RooRealVar("p1","p1", -113.6,-1000,1000) 		########################## This values are from previous fits, but any value works.
+	#p2 = RooRealVar("p2","p2", 56.39,-100,100)
+	#p3 = RooRealVar("p3","p3", 8.31,-100,100)
+	p1 = RooRealVar("p1","p1", 1,-1000,1000) 		########################## This values are from previous fits, but any value works.
+	p2 = RooRealVar("p2","p2", 1,-100,100)
+	p3 = RooRealVar("p3","p3", 1,-100,100)
 	bkgP4 = RooGenericPdf("bkgP4","bkg pdf","pow(1-x/8000.0,p1)/pow(x/8000.0,p2+p3*log(x/8000.))",RooArgList(x,p1,p2,p3))
 
 	inputFile = TFile(file)
 	h_Data = inputFile.Get(folder+'/' + histo)
 	data = RooDataHist("data_obs","data", quadruplets, h_Data)
-	nData = h_Data.Integral(36,120)
+	nData = h_Data.Integral(0,200)
 	nBkg = RooRealVar("nBkg","number of background events",nData,0,2*nData)
 	exPdfBkgP4 = RooExtendPdf("bkg","background PDF P4 fit",bkgP4,nBkg) 
 
 	#bkgP4.fitTo(data,RooFit.Strategy(2))
-	exPdfBkgP4.fitTo(data,RooFit.Strategy(2))
+	#fitRes = exPdfBkgP4.fitTo(data,RooFit.Strategy(2), RooFit.Save())
+	fitRes = exPdfBkgP4.fitTo(data, RooFit.Extended(), RooFit.Minos(False), RooFit.Hesse(False), RooFit.Save()  ) 	#### RooFitResults
+	fitRes.Print()
 
 	#### Create Workspace for RooFit              ####
-	pWs = RooWorkspace("myWS")
+	pWs = RooWorkspace("myDataWS")
 	# import dataset into workspace
 	getattr(pWs,'import')(data) # we call it this way because "import" is a reserved word in python
 	getattr(pWs,'import')(exPdfBkgP4)
 	nBkgDataCard = nBkg.getVal()
-	#print nBkgDataCard, nSigDataCard
+	print "################################## nBkg for Data Card :", nBkgDataCard
+	pWs.Print()
 	#save the workspace to a file
 	pWs.writeToFile(WSDir+WSname+'.root')
 
 	#### Plot Data
 	plotDataP4(data, exPdfBkgP4, histo, outputDir, WSDir, WSname)
 
+	print '#########################################', nBkg.getValV()
 	return p1.getValV(), p2.getValV(), p3.getValV(), p1.getError(), p2.getError(), p3.getError(), nBkg.getValV(), nBkg.getError()
 
 
@@ -86,7 +94,8 @@ def plotDataP4(data, exPdfBkgP4, histo, outputDir, WSDir, WSname):
 	dataFrame = x.frame()
 	dataFrame.SetTitle("")
 	dataFrame.GetYaxis().SetTitle("Quadruplets / 10 GeV")
-	data.plotOn( dataFrame, RooFit.DataError(RooAbsData.SumW2))
+	data.plotOn( dataFrame, RooFit.ProjectionRange('all'), RooFit.DataError(RooAbsData.SumW2))
+	#data.plotOn( dataFrame, RooFit.DataError(RooAbsData.SumW2))
 	exPdfBkgP4.plotOn( dataFrame )
 	exPdfBkgP4.paramOn( dataFrame, RooFit.Layout(0.65,0.85,0.5) )
 	dataFrame.getAttText().SetTextSize(0.0284)
@@ -108,7 +117,8 @@ def plotDataP4(data, exPdfBkgP4, histo, outputDir, WSDir, WSname):
 	dataFrame.Draw()
 	leg.Draw()
 	textBox.Draw()
-	cDataFitP4.SaveAs(outputDir+histo+"_data_FitP4.pdf")
+	cDataFitP4.SaveAs(outputDir+histo+"_data_FitP4_RooFit.pdf")
+	cDataFitP4.SaveAs(outputDir+histo+"_data_FitP4_RooFit.png")
 	del cDataFitP4
 	c2DataFitP4 = TCanvas("c2DataFitP4","data hist",800,600)
 	c2DataFitP4.SetLogy()
@@ -117,7 +127,8 @@ def plotDataP4(data, exPdfBkgP4, histo, outputDir, WSDir, WSname):
 	dataFrame.Draw()
 	leg.Draw()
 	textBox.Draw()
-	c2DataFitP4.SaveAs(outputDir+histo+"_data_FitP4_log.pdf")
+	c2DataFitP4.SaveAs(outputDir+histo+"_data_FitP4_log_RooFit.pdf")
+	c2DataFitP4.SaveAs(outputDir+histo+"_data_FitP4_log_RooFit.png")
 	del c2DataFitP4
 	print '|-------------- ChiSquare :', dataFrame.chiSquare()
 
@@ -129,7 +140,8 @@ def plotDataP4(data, exPdfBkgP4, histo, outputDir, WSDir, WSname):
 	cResidual = TCanvas("cResidual","Residual",800,600)
 	ResidualFrame.Draw()
 	textBox.Draw()
-	cResidual.SaveAs(outputDir+histo+"_data_FitP4_Residual.pdf")
+	cResidual.SaveAs(outputDir+histo+"_data_FitP4_Residual_RooFit.pdf")
+	cResidual.SaveAs(outputDir+histo+"_data_FitP4_Residual_RooFit.png")
 	del cResidual
 
 	hPull = RooHist(dataFrame.pullHist())
@@ -140,7 +152,8 @@ def plotDataP4(data, exPdfBkgP4, histo, outputDir, WSDir, WSname):
 	cPull = TCanvas("cPull","Pull",800,600)
 	PullFrame.Draw()
 	textBox.Draw()
-	cPull.SaveAs(outputDir+histo+"_data_FitP4_Pull.pdf")
+	cPull.SaveAs(outputDir+histo+"_data_FitP4_Pull_RooFit.pdf")
+	cPull.SaveAs(outputDir+histo+"_data_FitP4_Pull_RooFit.png")
 	del cPull
 
 	### Save histos in file
@@ -161,69 +174,61 @@ def plotResoBasedBin(file, h, folder, p1, p2, p3, p1Err, p2Err, p3Err, nBkg, nBk
 	h_Initial = inputFile.Get(folder+'/' + histo )
 	hFit = h_Initial.Clone(histo)
 	
-
-	######### Fit Functions
-	P4Fit = TF1("P4Fit", "[0]*pow(1-x/8000.0,[1])/pow(x/8000.0,[2]+[3]*log(x/8000.))",350,2000)
-	##print hFit.GetMaximumBin(), PreFitStart, PreFitEnd
-	p0 = nBkg/binSize/100
-	p0Err = nBkgErr/binSize/100
-
-	############################### Stop2
-	P4Fit.FixParameter(1,p1)
-	P4Fit.FixParameter(2,p2)
-	P4Fit.SetParError(2,p2Err)
-	P4Fit.FixParameter(3,p3)
-	P4Fit.SetParError(3,p3Err)
-	hFit.Fit(P4Fit,'B',"",FitStart,FitEnd)
-	P4Fit.SetParError(1,p1Err)
-	#hFit.Fit(P4Fit,"MRI","",FitStart,FitEnd)
-
 	################################ Pull and Residual Distro
-	alpha = 1 - 0.6827
-
 	hPull = h_Initial.Clone()
 	hResidual = h_Initial.Clone()
 
-	P4 = TF1("P4", "[0]*pow(1-x/8000.0,[1])/pow(x/8000.0,[2]+[3]*log(x/8000.))",350,2000)
-	P4.SetParameter(0,P4Fit.GetParameter(0))
-	P4.SetParameter(1,p1)
-	P4.SetParameter(2,p2)
-	P4.SetParameter(3,p3)
-	
-	listBinMass = []
-	listCorrectP4 = []
-	for bin in range(0, len(massBins)-1):
-		hPull.SetBinContent(bin, 0.)
-		hPull.SetBinError(bin, 0.)
-		hResidual.SetBinContent(bin+1, 0.)
-		hResidual.SetBinError(bin+1, 0.)
+	#listCorrectP4 = []
+	for ibin in range(0, len(massBins)-1):
+		hPull.SetBinContent(ibin, 0.)
+		hPull.SetBinError(ibin, 0.)
+		hResidual.SetBinContent(ibin+1, 0.)
+		hResidual.SetBinError(ibin+1, 0.)
 		
-		N = (h_Initial.GetBinContent(bin))
+		resoBasedBinSize = massBins[ibin + 1] - massBins[ibin]
+		hFit.SetBinContent( ibin, h_Initial.GetBinContent(ibin)/ resoBasedBinSize )
+		hFit.SetBinError( ibin, h_Initial.GetBinError(ibin)/ resoBasedBinSize )
+
+	######### Fit Functions
+	P4Fit = TF1("P4Fit", "[0]*pow(1-x/8000.0,[1])/pow(x/8000.0,[2]+[3]*log(x/8000.))",0,2000)
+	##print hFit.GetMaximumBin(), PreFitStart, PreFitEnd
+
+	############################### Stop2
+	#integral = h_Initial.Integral(11,200)
+	#integral = hFit.Integral(11,200)
+	#print integral
+	#P4Fit.SetParameter(0, nBkg/integral)
+	P4Fit.FixParameter(1,p1)
+	P4Fit.FixParameter(2,p2)
+	P4Fit.FixParameter(3,p3)
+	hFit.Fit(P4Fit,'B',"",FitStart,FitEnd)
+	#hFit.Fit(P4Fit,'B',"",FitStart,FitEnd)
+	#hFit.Fit(P4Fit,'B',"",FitStart,FitEnd)
+	#hFit.Fit(P4Fit,"MRI","",FitStart,FitEnd)
+
+		
+	#for bin in range(11, len(massBins)-1):
+	for bin in range(0, len(massBins)-1):
+		#N = (h_Initial.GetBinContent(bin))
+		N = (hFit.GetBinContent(bin))
 		binMiddlepoint = (massBins[bin] + massBins[bin+1])/2
-		valIntegral = (P4.Eval(binMiddlepoint))
-		print N, massBins[bin], massBins[bin+1], valIntegral, (massBins[bin] + massBins[bin+1])/2
-
-		if ( massBins[bin] > FitStart ):
-			sizeBin = massBins[bin+1] - massBins[bin]
-			correctValP4 = valIntegral*binSize/sizeBin
-			print '|-------------', bin, massBins[bin], sizeBin, correctValP4
-
+		valIntegral = (P4Fit.Eval(binMiddlepoint))
+		##print binMiddlepoint, valIntegral, N
+		#print N, massBins[bin], massBins[bin+1], valIntegral, (massBins[bin] + massBins[bin+1])/2
 
 
 		errDiff = sqrt(N)
 		diff = (N - valIntegral)/ valIntegral
 		#print N, errDiff
 		if (( massBins[bin] >= FitStart) and (N != 0) and ( massBins[bin] <= FitEnd) ):
-		#if (N != 0):
 			pull = (N - valIntegral)/ errDiff
 			#print bin*massBins[bin], N, valIntegral, errDiff, pull
 			hPull.SetBinContent(bin, pull)
 			hPull.SetBinError(bin, 1.0)
 	
-		#print diff
-		#if (( ibin >= FitStart/massBins[ibin]) and (N != 0) and (ibin <= FitEnd/massBins[ibin])):
+			#resError = diff * TMath.Sqrt( TMath.Power( P4Fit.GetParError(0) / P4Fit.GetParameter(0),2 ) + TMath.Power( p1Err/ p1, 2 )  + TMath.Power( p2Err/ p2, 2 )  + TMath.Power( p3Err/ p3, 2 ) )
 			hResidual.SetBinContent(bin, diff)
-			hResidual.SetBinError(bin, errDiff/valIntegral)
+			hResidual.SetBinError(bin, hFit.GetBinError(bin)/valIntegral )#resError )#errDiff/valIntegral)
 	############################################################################### 
 
 	##### Histo to save
@@ -282,6 +287,22 @@ def plotResoBasedBin(file, h, folder, p1, p2, p3, p1Err, p2Err, p3Err, nBkg, nBk
 	line = TGraph(2, xline, yline)
 	line.SetLineColor(kRed)
 
+	hPull.GetXaxis().SetTitle("")
+	hPull.GetYaxis().SetLabelSize(0.08)
+	hPull.GetXaxis().SetLabelSize(0.08)
+	hPull.GetYaxis().SetTitleSize(0.06)
+	hPull.GetYaxis().SetTitleOffset(0.6)
+	hPull.Sumw2()
+
+	hResidual.GetXaxis().SetTitle("")
+	hResidual.GetYaxis().SetLabelSize(0.08)
+	hResidual.GetXaxis().SetLabelSize(0.08)
+	hResidual.GetYaxis().SetTitleSize(0.06)
+	hResidual.GetYaxis().SetTitleOffset(0.6)
+	hResidual.SetMaximum(3)
+	hResidual.SetMinimum(-2)
+	#hResidual.Sumw2()
+
 	######### Plotting Histograms
 	c1 = TCanvas('c1', 'c1',  10, 10, 750, 1000 )
 	pad1 = TPad("pad1", "Fit",0,0.50,1.00,1.00,-1)
@@ -293,14 +314,14 @@ def plotResoBasedBin(file, h, folder, p1, p2, p3, p1Err, p2Err, p3Err, nBkg, nBk
 	
 	pad1.cd()
 	pad1.SetLogy()
-	gStyle.SetOptFit(1102)
+	gStyle.SetOptFit()
 	gStyle.SetStatY(0.9)
 	gStyle.SetStatX(0.9)
 	gStyle.SetStatW(0.15)
 	gStyle.SetStatH(0.13) 
 	hFit.Sumw2()
-	hFit.Draw("L")
-	#P4Fit.Draw("same")
+	hFit.Draw()
+	P4Fit.Draw("lsame")
 	legend.Draw()
 	textBox.Draw()
 	textBox3.Draw()
@@ -310,28 +331,16 @@ def plotResoBasedBin(file, h, folder, p1, p2, p3, p1Err, p2Err, p3Err, nBkg, nBk
 	
 	pad2.cd()
 	gStyle.SetOptStat(0)
-	hPull.GetXaxis().SetTitle("")
-	hPull.GetYaxis().SetLabelSize(0.08)
-	hPull.GetXaxis().SetLabelSize(0.08)
-	hPull.GetYaxis().SetTitleSize(0.06)
-	hPull.GetYaxis().SetTitleOffset(0.6)
-	hPull.Sumw2()
 	hPull.Draw("E")
 	line.Draw("L")
 	
 	pad3.cd()
 	gStyle.SetOptStat(0)
-	hResidual.GetXaxis().SetTitle("")
-	hResidual.GetYaxis().SetLabelSize(0.08)
-	hResidual.GetXaxis().SetLabelSize(0.08)
-	hResidual.GetYaxis().SetTitleSize(0.06)
-	hResidual.GetYaxis().SetTitleOffset(0.6)
-	#hResidual.Sumw2()
 	hResidual.Draw("e")
 	line.Draw("L")
 	
-	c1.SaveAs(outputDir+histo+"_data_FitP4Gauss.pdf")
-	c1.SaveAs(outputDir+histo+"_data_FitP4Gauss.png")
+	c1.SaveAs(outputDir+histo+"_data_FitP4_RooFit.pdf")
+	c1.SaveAs(outputDir+histo+"_data_FitP4_RooFit.png")
 	del c1
 	
 	c2 = TCanvas('c2', 'c2',  10, 10, 750, 500 )
@@ -340,8 +349,8 @@ def plotResoBasedBin(file, h, folder, p1, p2, p3, p1Err, p2Err, p3Err, nBkg, nBk
 	hPull.Draw("e")
 	line.Draw("L")
 	textBox.Draw("same")
-	c2.SaveAs(outputDir+histo+"_data_FitP4Gauss_Pull.pdf")
-	c2.SaveAs(outputDir+histo+"_data_FitP4Gauss_Pull.png")
+	c2.SaveAs(outputDir+histo+"_data_FitP4_Pull_RooFit.pdf")
+	c2.SaveAs(outputDir+histo+"_data_FitP4_Pull_RooFit.png")
 	del c2
 	
 	c3 = TCanvas('c3', 'c3',  10, 10, 750, 500 )
@@ -350,7 +359,8 @@ def plotResoBasedBin(file, h, folder, p1, p2, p3, p1Err, p2Err, p3Err, nBkg, nBk
 	hResidual.Draw("e")
 	line.Draw("L")
 	textBox.Draw("same")
-	c3.SaveAs(outputDir+histo+"_data_FitP4Gauss_Residual.pdf")
+	c3.SaveAs(outputDir+histo+"_data_FitP4_Residual_RooFit.pdf")
+	c3.SaveAs(outputDir+histo+"_data_FitP4_Residual_RooFit.png")
 	del c3
 	
 	c4 = TCanvas('c4', 'c4',  10, 10, 750, 500 )
@@ -363,21 +373,21 @@ def plotResoBasedBin(file, h, folder, p1, p2, p3, p1Err, p2Err, p3Err, nBkg, nBk
 	hFit.Sumw2()
 	hFit.Draw()
 	textBox.Draw("same")
-	
-	
-	c4.SaveAs(outputDir+histo+"_data_FitP4Gauss_FitOnly.pdf")
 	legend.Draw()
 	textBox.Draw()
 	textBox2.Draw()
 	textBox3.Draw()
 	textBox4.Draw()
 	textBox5.Draw()
+	
+	c4.SaveAs(outputDir+histo+"_data_FitP4_FitOnly_RooFit.pdf")
+	c4.SaveAs(outputDir+histo+"_data_FitP4_FitOnly_RooFit.png")
 	del c4
 
 if __name__ == '__main__':
 
 	###### Input parameters
-	histo = 'massRecoDiBjetDiJet_cutDiagStop2bbjj0' #str ( sys.argv[1] )
+	histo = 'massRecoDiBjetDiJet_cutDiagStop2bbjj0'#_resoBasedBin' #str ( sys.argv[1] )
 	folder = 'step3plots1D' #str ( sys.argv[2] )
 
 	####### Input files
